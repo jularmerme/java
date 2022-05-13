8823b6b5e3cb60ae3ef31059e5ba9ecf5ce5de1f
julianMercado <julianmercadomeza@gmail.com>
2022-05-13 14:21:00 -0400

    Initial Commit

---
+++ JavaCollections/src/collections/java/examples/ArrayListExamples.java
@@ -0,0 +1,11 @@
+package collections.java.examples;
+
+import java.util.*;
+
+public class ArrayListExamples {
+	ArrayList<Integer> al = new ArrayList<Integer>();
+	
+	for(int i=0; i <= 9; i++) {
+		al.add(i);
+	}
+}
---
+++ JavaCollections/src/collections/java/examples/MainDriver.java
@@ -0,0 +1,7 @@
+package collections.java.examples;
+
+public class MainDriver {
+	public static void main(String[] args) {
+		
+	}
+}
